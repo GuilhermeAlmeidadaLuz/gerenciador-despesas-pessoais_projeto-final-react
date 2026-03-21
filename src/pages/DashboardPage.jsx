acd0c382b8bd172fcf15b14a891c01187d1b83cc
Guilherme Almeida <guilhermealmeidadaluz5@gmail.com>
2026-03-21 17:48:46 -0400

    feat(pages): primeiro ajuste das páginas de detalhes da transação, dashboard e página inicial

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,1 +1,19 @@
 // Página que vai renderizar o componente Dashboard, Lista de transações finaneiras e outros que forem necessários
+import FinancialDashboard from "../components/FinancialDashboard"
+import FinancialTransactionsList from "../components/FinancialTransactionsList"
+
+import { Link } from 'react-router-dom'
+
+export default function DashboardPage() {
+    return (
+        <main className="dashboard-page">
+            <FinancialDashboard />
+            <div className="actions-container">
+                <Link to="/transacoes-financeiras/registrar" className="btn-action positive">
+                    Adicionar Nova Movimentação
+                </Link>
+            </div>
+            <FinancialTransactionsList />
+        </main>
+    )
+}
